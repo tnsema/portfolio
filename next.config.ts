2c6f10ec6d99d2a6304f8eb7cf4663a2aa737e97
Thobile Sema <thobilesema@gmail.com>
2026-01-18 16:25:23 +0200

    message

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio",
   trailingSlash: true,
-
-  env: {
-    NEXT_PUBLIC_BASE_PATH: "/portfolio",
-  },
 };
 
 export default nextConfig;
