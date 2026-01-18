cd7648aff0600ad308f50a310168fcdead55f30e
Thobile Sema <thobilesema@gmail.com>
2026-01-18 16:19:49 +0200

    pushshs

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-
-  // GitHub Pages repo site: https://tnsema.github.io/portfolio/
   basePath: "/portfolio",
   assetPrefix: "/portfolio",
-
-  // Recommended for GitHub Pages routing
   trailingSlash: true,
+
+  env: {
+    NEXT_PUBLIC_BASE_PATH: "/portfolio",
+  },
 };
 
 export default nextConfig;
